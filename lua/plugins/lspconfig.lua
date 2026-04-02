e0b319db7eae8b4f1c6ba26792ad239c69f168f3
Evgenii Dobrovidov <mr.nate.river@gmail.com>
2026-04-02 16:09:36 +0200

    Disable inlay hints by default

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -88,7 +88,8 @@ return {
 				--
 				-- This may be unwanted, since they displace some of your code
 				if client and client:supports_method("textDocument/inlayHint", event.buf) then
-					vim.lsp.inlay_hint.enable(true)
+				  -- Keep hints disabled by default
+					-- vim.lsp.inlay_hint.enable(true)
 
 					map("<leader>th", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
